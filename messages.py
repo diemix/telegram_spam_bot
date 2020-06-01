2420ba958771ec6068cc6d6802a63b4a9e49fa18
diemix <57945907+diemix@users.noreply.github.com>
2020-06-01 21:42:46 +0300

    Bug fixes

---
+++ messages.py
@@ -0,0 +1,3 @@
+def m_help(id):
+    message_text = "Telegram Bot. Id of this chat : " + str(id)
+    return message_text
--- spam.py
+++ spam.py
@@ -1,63 +1,50 @@
-#Создатель не несет отственности за ущерб или неудобства которые мог причинить бот.
-#The creator is not liable for damage or inconvenience that the bot could cause.
-
-#Use this for fun. Not for revenge.
 import time
 import telebot
 from telebot import types
 import random
 import string
+import configparser
+import os
+from messages import *
 
-def get_id(id):
-    id = int(id)
+def create_config(token, file):
+    config = configparser.ConfigParser()
+    config.add_section("info")
+    config.set("info", "token", token)
+
+    with open(file, "w") as config_file:
+        config.write(config_file)
+
+def set_up_bot():
+    if not os.path.exists("token.ini"):
+        token = input('Insert bot token..')
+        create_config(token, "token.ini")
+    else:
+        config = configparser.ConfigParser()
+        config.read("token.ini")
+        token = config.get("info", "token")
+    return token
+
+def spam():
+    id = input("Insert chat id.. ")
+    print("Spamming..")
     message_number = 0
     while True:
-        for counter in range(21):
-            send_message(id,message_number)
-            message_number+=1
-        time.sleep(15)
-
-def check_token(token):
-    try:
-        bot = telebot.TeleBot(token)
-    except:
-        token =input("Invalid token.. ")
-        check_token(token)
-    return bot
+        for counter in range(18):
+            message_number += 1
+            send_message(id, message_number)
+        time.sleep(10)
+        spam()
 
 def send_message(id, message_number):
-    try:
-        time.sleep(0.3)
-        text = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation + "/start" + "/help") for i in range(1000)])
-        keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=False)
-        button = types.KeyboardButton(text=text)
-        keyboard.add(button, button, button, button, button, button, button, button, button, button, button, button,
-                     button, button, button)
-        bot.send_message(id, text, reply_markup=keyboard)
-        print("Message number",message_number," sent to ", str(id), "        date & time: ", time.asctime())
-    except:
-        print("Failed to spam ", str(id), "Retrying in 20 secs..        date & time: ", time.asctime())
-        time.sleep(20)
+    time.sleep(0.3)
+    text = ''.join([random.choice(string.ascii_letters + string.digits + string.punctuation) for i in range(1000)])
+    keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=False)
+    button = types.KeyboardButton(text=text)
+    keyboard.add(button, button, button, button, button, button, button, button, button, button, button)
+    bot.send_message(id, text, reply_markup=keyboard)
+    print("Message number", message_number, " sent to ", str(id), "        date & time: ", time.asctime())
 
-def spam(id):
-    try:
-        id = message.text
-        id = int(id)
-        message_number = 0
-        while True:
-            for counter in range(21):
-                send_message(id,message_number)
-                message_number+=1
-            time.sleep(15)
-    except:
-        sleep(30)
-        spam(id)
-
-@bot.message_handlers(commands=['help'])
-def help(message):
-    bot.send_message(message.chat.id, "Telegram Bot. Id of this chat - " + message.chat.id)
-
-token = input("Insert token.. ")
-bot = check_token(token)
-id = input("Insert chat id..")
+bot = telebot.TeleBot(set_up_bot())
+spam()
 bot.polling()
